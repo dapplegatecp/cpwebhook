69f4f82a23eb9add54348a639f576b1a70dd87cb
Doug Applegate <dapplegate@cradlepoint.com>
2023-01-19 23:24:33 -0500

    now use mongodb as the db

--- app/main.py
+++ app/main.py
@@ -1,46 +1,69 @@
 import hmac
+import json
 import logging
 import os
-import json
 
-from tinydb import TinyDB
+from sse_starlette import EventSourceResponse
+import motor.motor_asyncio
+from pymongo.cursor import CursorType
 from dotenv import load_dotenv
 from fastapi import FastAPI, Request, Response, status
-from pathlib import Path
 
 load_dotenv()
 
 SECRET = os.environ.get("CPWEBHOOK_SECRET") or "secret_key"
 X_SIG = "x-cp-signature"
-DB_PATH = os.environ.get("CPWEBHOOK_DB_PATH") or "data"
 
 logger = logging.getLogger("uvicorn")
 logger.info("Secret Key is %s", SECRET)
 
 app = FastAPI(debug=True)
 
-table = None
+db = None
+
 
 @app.on_event("startup")
 async def database():
-    global table
-    Path(DB_PATH).mkdir(parents=True, exist_ok=True)
-    db = TinyDB(DB_PATH + "/data.json")  # create a new database named "data"
-    table = db.table('messages')
+    global db
+    mongo_user = os.environ.get("CPWEBHOOK_DB_USER") or "root"
+    mongo_pw = os.environ.get("CPWEBHOOOK_DB_PASS") or "example"
+    mongo_host = os.environ.get("CPWEBHOOK_DB_HOST") or "localhost"
+    mongo_url = f"mongodb://{mongo_user}:{mongo_pw}@{mongo_host}/?authSource=admin"
+    client = motor.motor_asyncio.AsyncIOMotorClient(mongo_url)
+    db = client.webhooks
+
 
 def create_hash(key, message):
     h = hmac.new(key=key.encode('utf-8'), msg=message, digestmod="sha256")
     message_digest = h.hexdigest()
     return message_digest
 
-def add_message(msg):
-    table.insert(json.loads(msg))
 
-@app.get("/messages", status_code=200)
+async def add_message(msg):
+    await db.alerts.insert_one(msg)
+
+
+@app.get("/messages", status_code=status.HTTP_200_OK)
 async def messages():
-    return table.all()
+    rval = []
+    async for m in db.alerts.find({}):
+        m['_id'] = str(m['_id'])
+        rval.append(m)
+    return rval
 
-@app.post("/", status_code=403)
+
+@app.get('/stream', status_code=status.HTTP_200_OK)
+async def stream():
+    async def stream_data():
+        cursor = db.alerts.find({},cursor_type=CursorType.TAILABLE_AWAIT)
+        while cursor.alive:
+            async for m in cursor:
+                m['_id'] = str(m['_id'])
+                yield json.dumps(m)
+    return EventSourceResponse(stream_data())
+
+
+@app.post("/messages", status_code=403)
 async def root(request: Request, response: Response):
     msg_body = await request.body()
     logger.info("Body: %s", msg_body)
@@ -53,8 +76,14 @@ async def root(request: Request, response: Response):
     logger.info("sig: %s", sig)
 
     if xsig == sig:
+        try:
+            msg = json.loads(msg_body.decode("utf8"))
+        except ValueError as e:
+            logger.exception(e)
+            response.status_code = status.HTTP_400_BAD_REQUEST
+            return {"status": "bad request"}
         response.status_code = status.HTTP_200_OK
-        add_message(msg_body.decode("utf8"))
+        await add_message(msg)
         return {"status": "OK"}
     
-    return {"status": "failed"}
+    return {"status": "forbidden"}
