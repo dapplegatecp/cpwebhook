2ef00368eb443f496533a25581bfea820eecf68d
Doug Applegate <dapplegate@cradlepoint.com>
2023-01-22 17:51:40 -0500

    frontend and reorganize

---
+++ frontend/www/grid.js
@@ -0,0 +1,16 @@
+var source = new EventSource("https://webhook.crdlpt.com/stream");
+source.onmessage = function(event) {
+  var table = $("#table-data");
+  var data = JSON.parse(event.data);
+  var id = data._id
+  var time = data.detected_at
+  var type = data.type
+  var message = data.message
+  var row = $("<tr>");
+  var cell1 = $("<td>").text(id);
+  var cell2 = $("<td>").text(time);
+  var cell3 = $("<td>").text(type);
+  var cell4 = $("<td>").text(message);
+  row.append(cell1, cell2, cell3, cell4);
+  table.append(row);
+};
